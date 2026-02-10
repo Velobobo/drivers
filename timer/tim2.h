72df4ebf78152e3a78da10fe4ac8583f76054f5a
Velobobo <212455055+Velobobo@users.noreply.github.com>
2026-02-10 23:14:04 +0530

    Create tim2.h

---
+++ timer/tim2.h
@@ -0,0 +1,13 @@
+/*
+ * tim.h
+ *
+ *  Created on: Jan 11, 2026
+ *      Author: parth
+ */
+
+#ifndef TIM_H_
+#define TIM_H_
+void tim2_1hz_init(void);
+void tim2_1hz_interrupt_init(void);
+
+#endif /* TIM_H_ */
